2f61c2fc1022752d5dcf24d3f5c1ebb56953df5a
syoji-haruki <syojiharuki@shoujiharutakanoMacBook-Pro.local>
2019-03-14 15:08:23 +0900

    非同期通信完成

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,52 @@
+$(function(){
+  function buildHTML(message){
+    var image = `<img src="${message.image}">` if message.image.present?
+    var html = `<div class="content__message__main__box">
+  <div class="content__message__main__box__top">
+    <p class="content__message__main__box__top__talker">
+      ${message.user_name}
+    </p>
+    <p class="content__message__main__box__top__date">
+      ${message.created_at}
+    </p>
+  </div>
+  <p class="content__message__main__box__text">
+      <p class="lower-message__content">
+        ${message.content}
+      </p>
+      ${image}
+  </p>
+</div>
+`
+    return html;
+  }
+
+  $('#message_form').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.content__message__main').append(html)
+      $("#message_form")[0].reset();
+    })
+
+    .fail(function(){
+      alert('メッセージを入力して下さい');
+    })
+    .always(() => {
+    $(".form__submit").removeAttr("disabled");
+    });
+    $('.content__message__main').animate({scrollTop: $('.content__message__main')[0].scrollHeight}, 'fast');
+  })
+})
